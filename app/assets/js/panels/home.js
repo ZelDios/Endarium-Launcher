b6c3eedbe8d926b8fd6fefec0fd0e7651ffc942b
ZelDios <zeldios12@gmail.com>
2022-02-17 14:21:17 +0100

    dark

--- app/assets/js/panels/home.js
+++ app/assets/js/panels/home.js
@@ -2,6 +2,17 @@ import ("./home/start_game.js")
 import ("./home/status_server.js")
 import ("./home/news_server.js")
 
+
+function tab(info) {
+    let content = document.getElementsByClassName("tabsettings");
+    for (let i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+    document.querySelector(`.${info}`).style.display = "block";
+}
+
 document.querySelector(".settings-btn").addEventListener("click", () => {
     changePanel("home", "settings")
+    tab('ramsettinstab')
+    import ("./settings/java-memory.js")
 })
